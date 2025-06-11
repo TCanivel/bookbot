204363c6a48835a47eb0334496108e6ff5dff5a2
TCanivel <timothy.canivel@gmail.com>
2025-06-11 16:19:29 -0500

    Adding Letter Count Dictionairy

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 from stats import get_num_words
+
+from stats import charc_and_count
--- stats.py
+++ stats.py
@@ -11,8 +11,23 @@ def get_num_words(file_contents):
     num_words = len(words)
     print(f"{num_words} words found in the document")
 
+def charc_and_count(file_contents):
+    charc_count_dict = {}
+    words = file_contents.split()
+    for word in words:
+        lower_case_word = word.lower()
+        for letter in lower_case_word:
+            if letter not in charc_count_dict:
+                charc_count_dict[letter] = 1
+            else:
+                charc_count_dict[letter] += 1
+    print(charc_count_dict)
+            
+
+
 def main():
     file_contents = get_book_text(path_to_file)
     get_num_words(file_contents)
+    charc_and_count(file_contents)
 
 main()
