2fbb0f4c55addffd57c2b5a6c5f7f7a49b3a7ad1
TCanivel <timothy.canivel@gmail.com>
2025-06-11 21:27:24 -0500

    Final Printout

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from stats import path_to_file, get_book_text, charc_and_count
+from stats import path_to_file, get_book_text, charc_and_count, sort_on, dict_organize, get_num_words
 file_contents = get_book_text(path_to_file)
-dictionary_of_chars_and_counts = charc_and_count(file_contents)
-print(dictionary_of_chars_and_counts)
+num_words = get_num_words(file_contents)
+list_of_dict = dict_organize(file_contents)
+
+
+print("============ BOOKBOT ============")
+print("Analyzing book found at books/frankenstein.txt...")
+print("----------- Word Count ----------")
+print(f"Found {num_words} total words")
+print("--------- Character Count -------")
+for letter in list_of_dict:
+    print(f"{letter['char']}: {letter['num']}")
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -2,14 +2,13 @@ path_to_file = "books/frankenstein.txt"
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
-        file_contents = f.read()
-    
+        file_contents = f.read()  
     return file_contents
 
 def get_num_words(file_contents):
     words = file_contents.split()
     num_words = len(words)
-    print(f"{num_words} words found in the document")
+    return num_words
 
 def charc_and_count(file_contents):
     charc_count_dict = {}
@@ -17,17 +16,30 @@ def charc_and_count(file_contents):
     for word in words:
         lower_case_word = word.lower()
         for letter in lower_case_word:
+            if letter.isalpha() != True:
+                continue
             if letter not in charc_count_dict:
                 charc_count_dict[letter] = 1
             else:
                 charc_count_dict[letter] += 1
     return charc_count_dict
-            
 
+def sort_on(dict):
+    return dict["num"]
+
+def dict_organize(file_contents):
+    list_of_dict = []
+    dict_to_organize = charc_and_count(file_contents)
+    for letter, count in dict_to_organize.items():
+        diction = {"char" : letter, "num" : count}
+        list_of_dict.append(diction)
+    list_of_dict.sort(reverse = True, key = sort_on)
+    return list_of_dict
 
 def main():
     file_contents = get_book_text(path_to_file)
     get_num_words(file_contents)
     charc_and_count(file_contents)
+    dict_organize(file_contents)
 
 main()
